747dc55fc1ce85365d0a8791c82226486168fa6d
Paul Lewis <paulofmandown@gmail.com>
2014-09-07 23:08:14 -0500

    Update docs

--- src/feature.lua
+++
@@ -1,9 +0,0 @@
-local Feature_PATH =({...})[1]:gsub("[%.\\/]feature$", "") .. '/'
-local class  =require (Feature_PATH .. 'vendor/30log')
-
-local Feature = class { __name='Feature' }
-function Feature:isValid(gen, canBeDugCallback) end
-function Feature:create(gen, digCallback) end
-function Feature:debug() end
-function Feature:createRandomAt(x, y, dx, dy, options) end
-return Feature
