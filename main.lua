3ea5b9f5a0f3cf62cf15028e5c5b814168d09999
doyousketch2 <doyousketch2@yahoo.com>
2017-05-22 22:22:45 -0400

    Update main.lua
    Prints module names to the console.

--- main.lua
+++ main.lua
@@ -1,13 +1,10 @@
 --- rotLove Demo runner
 -- specify which example you'd like to see on the command line
---
--- to list all available examples, do:
---    love . list
 
 if arg[2] == "rtd" then
 	local mod = love.filesystem.getDirectoryItems("examples")
 	for i=#mod, 1, -1 do
-		if not mod[i]:match("%.lua$") then 
+		if not mod[i]:match("%.lua$") then
 			table.remove(mod, i)
 		else
 			mod[i] = mod[i]:gsub("%.lua$", "")
@@ -21,12 +18,29 @@ end
 if arg[2] then
 	require("examples.".. arg[2])
 else
+	local mod = love.filesystem.getDirectoryItems("examples")
 	io.write([[
+
 ERROR: Please specify a demo, for example:
 	love . preciseWithMovingPlayer
 you can get a random one using
 	love . rtd
-]])
 
+]])
+	for i=1, #mod do
+		modname = mod[i]
+		if modname:match("%.lua$") then
+			modname = string.sub(modname, 1, -5)
+			if i % 2 == 0 then
+				io.write(modname..'\n')
+			else
+				while #modname < 30 do
+					modname = modname .. ' '
+				end
+				io.write(modname)
+			end
+		end
+	end
+	io.write('\n\n')
 	love.event.push('quit')
 end
