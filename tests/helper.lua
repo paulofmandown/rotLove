0e5e5f26d7958b8f68317b1a6eed7bc312e6c930
airstruck <airstruck>
2017-07-12 17:19:56 -0400

    add eventqueue tests

--- tests/helper.lua
+++ tests/helper.lua
@@ -4,4 +4,6 @@ say:set("assertion.gt.positive", "Expected %s to be greater than %s")
 say:set("assertion.gt.negative", "Expected %s not to be greater than %s")
 say:set("assertion.lt.positive", "Expected %s to be less than %s")
 say:set("assertion.lt.negative", "Expected %s not to be less than %s")
+say:set("assertion.undef.positive", "Expected %s to be nil")
+say:set("assertion.undef.negative", "Expected %s not to be nil")
 
