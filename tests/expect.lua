71fb90819d4869a8659bb34b1b91634b79e46836
airstruck <airstruck>
2017-07-12 02:32:27 -0400

    add rng tests and fix a bug

--- tests/expect.lua
+++ tests/expect.lua
@@ -4,6 +4,14 @@ return function (assert)
         return {
             toEqual = function (b) return assert.are.same(b, a) end,
             toBe = function (b) return assert.are.equal(b, a) end,
+            toBeGreaterThan = function (b) return assert.is_true(a > b) end,
+            toBeLessThan = function (b) return assert.is_true(a < b) end,
+            NOT = {
+                toEqual = function (b) return assert.are_not.same(b, a) end,
+                toBe = function (b) return assert.are_not.equal(b, a) end,
+                toBeGreaterThan = function (b) return assert.is_not(a > b) end,
+                toBeLessThan = function (b) return assert.is_not(a < b) end,
+            }
         }
     end
 end
