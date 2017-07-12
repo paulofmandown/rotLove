a055eb7c00ac239f859c68df38b4826be64abd43
airstruck <airstruck>
2017-07-12 16:24:19 -0400

    add dungeon tests - brogue modified to pass corridors test - brogue doesn't pass >0 doors per room test, disabled that one

--- tests/expect.lua
+++ tests/expect.lua
@@ -1,16 +1,23 @@
 -- Some of Jasmine's API mapped onto Busted. Makes tests easier to port.
+
+
+local function gt (_, args) return args[1] > args[2] end
+local function lt (_, args) return args[1] < args[2] end
+
 return function (assert)
+    assert:register("assertion", "gt", gt, "assertion.gt.positive", "assertion.gt.negative")
+    assert:register("assertion", "lt", lt, "assertion.lt.positive", "assertion.lt.negative")
     return function (a)
         return {
-            toEqual = function (b) return assert.are.same(b, a) end,
-            toBe = function (b) return assert.are.equal(b, a) end,
-            toBeGreaterThan = function (b) return assert.is_true(a > b) end,
-            toBeLessThan = function (b) return assert.is_true(a < b) end,
+            toEqual = function (b) return assert.is.same(b, a) end,
+            toBe = function (b) return assert.is.equal(b, a) end,
+            toBeGreaterThan = function (b) return assert.gt(a, b) end,
+            toBeLessThan = function (b) return assert.lt(a, b) end,
             NOT = {
-                toEqual = function (b) return assert.are_not.same(b, a) end,
-                toBe = function (b) return assert.are_not.equal(b, a) end,
-                toBeGreaterThan = function (b) return assert.is_not(a > b) end,
-                toBeLessThan = function (b) return assert.is_not(a < b) end,
+                toEqual = function (b) return assert.Not.same(b, a) end,
+                toBe = function (b) return assert.Not.equal(b, a) end,
+                toBeGreaterThan = function (b) return assert.Not.gt(a, b) end,
+                toBeLessThan = function (b) return assert.Not.lt(a, b) end,
             }
         }
     end
---
+++ tests/helper.lua
@@ -0,0 +1,7 @@
+local say = require 'say'
+
+say:set("assertion.gt.positive", "Expected %s to be greater than %s")
+say:set("assertion.gt.negative", "Expected %s not to be greater than %s")
+say:set("assertion.lt.positive", "Expected %s to be less than %s")
+say:set("assertion.lt.negative", "Expected %s not to be less than %s")
+
